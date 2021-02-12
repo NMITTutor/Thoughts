e85d8a82cd3312ddaf7a37b671eb12f6918cdf9e
NMITTutor <Todd.Cochrane@nmit.ac.nz>
2021-02-13 10:16:23 +1300

    Playing with classes

--- TypeScriptFizzBuzz/TypeScriptFizzBuzz.ts
+++ TypeScriptFizzBuzz/TypeScriptFizzBuzz.ts
@@ -1,3 +1,37 @@
 let a: string = "Hello, Nothing here yet";
 
 console.log(a);
+enum FizzSelector {
+   Value, FizzBuz,
+}
+interface AFizzBuzz {
+    value : number;
+    fizzbuzz : string;
+    showMe : (which? : FizzSelector) => void ,
+}
+
+class FizzBuzz implements AFizzBuzz {
+    value : number;
+    fizzbuzz : string;
+    showMe = (which? : FizzSelector) => 
+    {
+        if(which === FizzSelector.Value ) {
+            console.log(this.value);
+        }
+        else if(which === FizzSelector.FizzBuz ) {
+            console.log(this.fizzbuzz);
+        }
+        else {
+            console.log(this.value, this.fizzbuzz);
+        }
+        
+
+    } 
+     
+    constructor(value :number, fizzbuzz : string){
+
+    }
+}
+
+
+
